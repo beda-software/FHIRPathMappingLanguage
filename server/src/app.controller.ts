d0d476586fc3c558f25134e464b49803d429c3f2
Ilya Beda <ir4y.ix@gmail.com>
2023-09-04 15:12:28 +1000

    Add JS reference implementation

---
+++ server/src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Resource } from 'fhir/r4b';
+
+
+class Template {
+    context: Resource;
+    template: object;
+}
+
+@Controller()
+export class AppController {
+    constructor(private readonly appService: AppService) {}
+
+    @Post()
+    @HttpCode(200)
+    resolveTemplate(@Body() body: Template): object {
+        const {context, template} = body;
+        return this.appService.resolveTemplate(context, template);
+    }
+}
---
+++ server/src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { resolveTemplate } from './utils/extract'
+import { Resource } from 'fhir/r4b';
+
+@Injectable()
+export class AppService {
+    resolveTemplate(qr: Resource, template: object): object {
+        return resolveTemplate(qr,template)
+    }
+}
