d0e1edb4fcdcdee8bb1832d06526f5803e982dd3
Ilya Beda <ir4y.ix@gmail.com>
2023-10-10 20:32:17 +1100

    Add python examples

---
+++ python/bs_repeatable.py
@@ -0,0 +1,54 @@
+from fhirpathpy import dsl
+
+from .resources import (
+    Bundle,
+    BundleEntry,
+    BundleEntryRequest,
+    ContactPoint,
+    Patient,
+    QuestionnaireResponse,
+    QuestionnaireResponseItem,
+)
+
+
+def apply(s, qr):
+    return s
+
+
+def map(qr: QuestionnaireResponse) -> Bundle:
+    return apply(
+        Bundle(
+            type="transaction",
+            entry=[
+                BundleEntry(
+                    request=BundleEntryRequest(
+                        url="/Patient/"
+                        + dsl.QuestionnaireResponse.repeat("item")
+                        .where(linkId="patientId")
+                        .answer.valueSteing,
+                        method="PATCH",
+                    ),
+                    resource=Patient(
+                        telecom=list(
+                            mapItem(c)
+                            for c in dsl.QuestionnaireResponse.repeat("item").where(
+                                linkId="phone-group"
+                            )
+                        )
+                    ),
+                )
+            ],
+        ),
+        qr,
+    )
+
+
+def mapItem(qi: QuestionnaireResponseItem) -> ContactPoint:
+    return apply(
+        ContactPoint(
+            use=dsl.where(linkId="phone-type").answer.valueString,
+            value=dsl.where(linkId="phone-value").answer.valueString,
+            system="phone",
+        ),
+        qi,
+    )
---
+++ python/pydian_repeatable.py
@@ -0,0 +1,27 @@
+from pydian import DROP, Mapper, get
+
+
+def map(qr):
+    return {
+        "resourceType": "Bundle",
+        "type": "transaction",
+        "entry": [
+            {
+                "request": {
+                    "url": "/Patient/" + get(qr, "item[?linkId == 'patientId'].answer.valueString"),
+                    "method": "Patch",
+                },
+                "resource": {
+                    "resourceType": "Patient",
+                    "telecom": list(
+                        {
+                            "use": get(item, "[?linkId == 'phone-type'].answer.valueString"),
+                            "value": get(item, "[?linkId == 'phone-number'].answer.valueString"),
+                            "system": "phone",
+                        }
+                        for item in get(qr, "item[?linkId == 'phone-group']")
+                    ),
+                },
+            }
+        ],
+    }
