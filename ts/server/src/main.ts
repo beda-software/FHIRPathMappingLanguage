8d81914b69ec89269949bb54c20e078559c79ab0
Vadim Laletin <rusmiligamer@gmail.com>
2024-07-04 17:56:34 +0200

    Increase json payload limit to 5mb

--- ts/server/src/main.ts
+++ ts/server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    app.use(json({ limit: '5mb' }));
     await app.listen(8090);
 }
 bootstrap();
